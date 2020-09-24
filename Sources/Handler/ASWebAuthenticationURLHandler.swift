0b5bca95b6608dba7460fa838565bf8d87e61525
Andrew Ishchenko <aishchenko@readdle.com>
2020-09-24 14:25:28 +0300

    - Add macOS compatibility to ASWebAuthenticationURLHandler.

--- Sources/Handler/ASWebAuthenticationURLHandler.swift
+++ Sources/Handler/ASWebAuthenticationURLHandler.swift
@@ -9,11 +9,12 @@
 import Foundation
 #if os(iOS)
 import SafariServices
+#endif
 #if canImport(AuthenticationServices)
 import AuthenticationServices
 #endif
 
-@available(iOS 13.0, macCatalyst 13.0, *)
+@available(iOS 13.0, OSX 10.15, macCatalyst 13.0, *)
 open class ASWebAuthenticationURLHandler: OAuthSwiftURLHandlerType {
     var webAuthSession: ASWebAuthenticationSession!
     let callbackUrlScheme: String
@@ -31,23 +32,30 @@ open class ASWebAuthenticationURLHandler: OAuthSwiftURLHandlerType {
                                                     completionHandler: { callback, error in
                                                         guard error == nil, let successURL = callback else {
                                                             let msg = error?.localizedDescription.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
-                                                            let urlString = "\(self.callbackUrlScheme)?error=\(msg ?? "UNKNOWN")"
+                                                            let urlString = "\(self.callbackUrlScheme)://?error=\(msg ?? "UNKNOWN")"
                                                             guard let url = URL(string: urlString) else {
                                                                 return
                                                             }
-                                                            #if !OAUTH_APP_EXTENSIONS
-                                                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                                                            #endif
+                                                            
+                                                            Self.handle(url: url)
                                                             return
                                                         }
-                                                        #if !OAUTH_APP_EXTENSIONS
-                                                        UIApplication.shared.open(successURL, options: [:], completionHandler: nil)
-                                                        #endif
+                                                        
+                                                        Self.handle(url: successURL)
         })
         webAuthSession.presentationContextProvider = presentationContextProvider
 
         _ = webAuthSession.start()
     }
+    
+    private static func handle(url: URL) {
+        #if os(iOS) && !OAUTH_APP_EXTENSIONS
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        #else
+        // The iOS counterpart requires developers to register custom URL scheme for app and forward throught system available OAuth parameters.
+        // Maybe it makes sence to unify flow if you already added such scheme.
+        // But, I do not see any reason for us to do this.
+        OAuthSwift.handle(url: url)
+        #endif
+    }
 }
-
-#endif
